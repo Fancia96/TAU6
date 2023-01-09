ca88bdfd9d35e90bb4d3b23313b5ae4b1d626ad9
Fancia666 <s18425@pjwstk.edu.pl>
2023-01-09 12:42:07 +0100

    push file

--- src/main/java/org/example/Note.java
+++ src/main/java/org/example/Note.java
@@ -2,13 +2,19 @@ package org.example;
 
 public class Note {
 
+    private final String name;
     private final float note;
 
-    public Note(float note) {
+    public Note(String name, float note) {
+        this.name = name;
         this.note = note;
     }
 
     public float getNote() {
         return this.note;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
--- src/test/java/NotesServiceImplTest.java
+++ src/test/java/NotesServiceImplTest.java
@@ -1,14 +1,16 @@
+import org.example.MockSystemNotesService;
 import org.example.Note;
 import org.example.NotesServiceImpl;
 import org.junit.jupiter.api.*;
 
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class NotesServiceImplTest {
 
+    MockSystemNotesService env;
     public NotesServiceImpl notesServiceImpl;
     static Logger log = Logger.getLogger(String.class.getName());
 
@@ -26,7 +28,10 @@ public class NotesServiceImplTest {
     @BeforeEach
     void setUp() throws Exception {
         log.info("BeforeEach");
-        notesServiceImpl = new NotesServiceImpl(null);
+
+        env = new MockSystemNotesService();
+        notesServiceImpl = new NotesServiceImpl(env);
+
     }
 
     @AfterEach
@@ -37,15 +42,33 @@ public class NotesServiceImplTest {
 
 
     @Test
-    void testIsPalindromeEmptyString() {
+    void testIsItAClass() {
         assertEquals(notesServiceImpl.getClass().toString(), "class org.example.NotesServiceImpl");
     }
+
+    @Test
+    void testAddingANote() {
+        Note note = new Note("note 1", 44.5F);
+        notesServiceImpl.add(note);
+
+        assertTrue(env.checkIfNoteAdded(note));
+    }
+
+    @Test
+    void testAddingANoteAndNotFindingIt() {
+        Note note = new Note("note 1", 44.5F);
+        Note note2 = new Note("note 1", 43.5F);
+        notesServiceImpl.add(note);
+
+        assertFalse(env.checkIfNoteAdded(note2));
+    }
+//
 //
 //    @Test
 //    void testIsPalindromeWithTwoSpaces() {
 //        assertTrue(stringOperations.isPalindrome("a bcb a"));
 //    }
-//
+
 //    @Test
 //    void testConcatTwoZeroLenghtStrings() {
 //        assertEquals( null , stringOperations.concat("", ""));
